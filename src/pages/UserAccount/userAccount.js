4c5c72fc75f0d577b140ad097240ca722461687e
abrar0786 <theibrar1000@gmail.com>
2023-04-11 14:04:13 +0500

    setup basic foldering structure and starter of ecoomerce project

---
+++ src/pages/UserAccount/userAccount.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../../components/MainHeader/firstHeader/Header";
+import Card from "../../components/common/UI/Card/card";
+
+export default function userAccount() {
+  return (
+    // Card style={{ margin: "0px 20px" }}
+    <React.Fragment>
+      <Header />
+    </React.Fragment>
+  );
+}
